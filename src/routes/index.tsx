b285346563f449c033bdb41f52420395b61ec5ea
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-10-01 20:41:05 -0300

    show notification inapp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,10 @@
-import { useTheme } from 'native-base';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { useTheme } from "native-base";
+import { useEffect, useState } from "react";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { OneSignal, OSNotification } from "react-native-onesignal";
 
-import { AppRoutes } from './app.routes';
+import { Notification } from "../components/Notification";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const { colors } = useTheme();
@@ -9,9 +12,28 @@ export function Routes() {
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+  const [notification, setNotification] = useState<OSNotification>();
+  useEffect(() => {
+    OneSignal.Notifications.addEventListener(
+      "foregroundWillDisplay",
+      (event) => {
+        const response = event.getNotification();
+        setNotification(response);
+      }
+    );
+  }, []);
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+
+      {notification && (
+        <Notification
+          data={notification}
+          onClose={() => {
+            setNotification(undefined);
+          }}
+        />
+      )}
     </NavigationContainer>
   );
 }
