db9b1fa3ebf9319dc243acd7c0cee455ceac7ca0
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-10-02 21:56:54 -0300

    action Id

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from "react-native";
+import { useEffect } from "react";
 import { OneSignal } from "react-native-onesignal";
 import { NativeBaseProvider } from "native-base";
 import {
@@ -14,11 +15,22 @@ import { Loading } from "./src/components/Loading";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
 
+import { tagUserInfoCreate } from "./src/notifications/notificationsTags";
+
 OneSignal.initialize("ec574238-acef-43c2-ad69-7e78bb3e051b");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-
+  tagUserInfoCreate();
+  useEffect(() => {
+    const unsubscribe = OneSignal.Notifications.addEventListener(
+      "click",
+      (response) => {
+        const { url } = response.result;
+      }
+    );
+    return () => unsubscribe;
+  }, []);
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
