efee828a58b893364d3048b17982feafa333e58e
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-10-02 21:55:28 -0300

    deep linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,34 @@ import { useTheme } from "native-base";
 import { useEffect, useState } from "react";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { OneSignal, OSNotification } from "react-native-onesignal";
-
 import { Notification } from "../components/Notification";
 import { AppRoutes } from "./app.routes";
 
+const linking = {
+  prefixes: [
+    "igniteshoesapp://",
+    "com.erivaldo.igniteshoes://",
+    "exp+igniteshoesapp://",
+  ],
+  config: {
+    screens: {
+      details: {
+        path: "details/:productId",
+        parse: {
+          productId: (productId: string) => productId,
+        },
+      },
+    },
+  },
+};
+
 export function Routes() {
+  const [notification, setNotification] = useState<OSNotification>();
   const { colors } = useTheme();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
-  const [notification, setNotification] = useState<OSNotification>();
   useEffect(() => {
     OneSignal.Notifications.addEventListener(
       "foregroundWillDisplay",
@@ -23,7 +40,7 @@ export function Routes() {
     );
   }, []);
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
 
       {notification && (
